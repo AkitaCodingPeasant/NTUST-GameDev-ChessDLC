b0ec3eacb18285fd8409f20a008eb14977a7ce99
AkitaCodingPeasant <162419584+AkitaCodingPeasant@users.noreply.github.com>
2024-12-03 01:36:14 +0800

    DEPRECATE: Remove Consolas Debugger

--- ChessDLC/Program.cs
+++ ChessDLC/Program.cs
@@ -10,14 +10,14 @@ namespace ChessDLC {
         /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        private static extern bool AllocConsole();
+        // [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        // private static extern bool AllocConsole();
 
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            AllocConsole();
+            // AllocConsole();
             Console.WriteLine("Program is running...");
 
             Application.Run(new ChessDlcForm());
